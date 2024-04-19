f4485c4abaf7c6be5ed3f63ef66a1e68cf85f92d
itsbasil <itsbasilgeorge@gmail.com>
2024-04-20 00:40:45 +0530

    connection completed

--- myenv/Dictionary_Pjt/dictionary/views.py
+++ myenv/Dictionary_Pjt/dictionary/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from PyDictionary import PyDictionary
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 
-def search(request):
+def word(request):
+    search = request.GET.get('search') 
+    dictionary=PyDictionary() 
+    meaning=dictionary.meaning(search) 
+    synonyms=dictionary.synonym(search) 
+    antonyms=dictionary.antonym(search)
+    context = {
+        'meaning': meaning['Noun'][0],
+        'synonym': synonyms,
+        'antonym': antonyms,
+    }
 
-    return render(request,'search.html')
+    return render(request,'word.html',context)
